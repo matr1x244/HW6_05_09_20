d1f2ae52385d6237ca34e94c3e133a60782055be
matr1x244 <zio-21@yandex.ru>
2020-09-06 17:11:21 +0300

    06 09 20 hw6

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,101 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
-    
+    private final static String SERVER_IP = "localhost";
+    private final static int SERVER_PORT = 8181;
+
+    private static Socket socket;
+    private static DataInputStream in;
+    private static DataOutputStream out;
+
+    private static boolean isRunning; // true; false;
+
+    public static void main(String[] args) {
+        try {
+            connectServer ();
+            closeConnection ();
+        } catch (IOException e) {
+            e.printStackTrace ();
+        }
+    }
+
+    private static void connectServer() throws IOException {
+        socket = new Socket (SERVER_IP, SERVER_PORT);
+        in = new DataInputStream (socket.getInputStream ());
+        out = new DataOutputStream (socket.getOutputStream ());
+
+        isRunning = true;
+
+        // действия потоков
+
+        Thread OneIn = new Thread (() -> { // поток
+            try {
+                while (isRunning) {
+                    if (!socket.isConnected ()) {
+                        System.out.println ("Сервер закрылся");
+                        isRunning = false;
+                        break;
+                    }
+
+                    String strFromServer = in.readUTF ();
+
+                    if (strFromServer.equalsIgnoreCase ("/end")) {
+                        System.out.println ("Сервер закрылся");
+                        isRunning = false;
+                        break;
+                    }
+
+                    System.out.println ("Сервер пишет: " + strFromServer);
+                }
+            } catch (Exception e) {
+                e.printStackTrace ();
+            }
+        });
+        Thread TwoOut = new Thread (() -> { // поток
+            Scanner sc = new Scanner (System.in);
+            try {
+                while (isRunning) {
+                    if (!socket.isConnected ()) {
+                        isRunning = false;
+                        break;
+                    }
+
+                    String str = sc.nextLine ();
+                    out.writeUTF (str);
+
+                    if (str.equals ("/end")) {
+                        isRunning = false;
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace ();
+            }
+        });
+
+        //запуск потоков
+
+        OneIn.start ();
+        TwoOut.start ();
+
+        try {
+            OneIn.join (); // join пока кто то не напишет
+            TwoOut.join ();
+        } catch (InterruptedException e) {
+            e.printStackTrace ();
+        }
+    }
+
+    public static void closeConnection() throws IOException {
+        in.close ();
+        out.close ();
+
+        if (!socket.isClosed ()) {
+            socket.close ();
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,104 @@
-public class Server {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Server {
+    private static ServerSocket serverSocket;
+    private static Socket socket;
+    private static DataInputStream in;
+    private static DataOutputStream out;
+
+    private static boolean isRunning;
+
+    public static void main(String[] args) {
+        try {
+            startServer ();
+            closeConnection ();
+        } catch (IOException e) {
+            e.printStackTrace ();
+        }
+    }
+
+    private static void startServer() throws IOException {
+        socket = null;
+        serverSocket = new ServerSocket (8181);
+        System.out.println ("Сервер запущен");
+        socket = serverSocket.accept ();
+        in = new DataInputStream (socket.getInputStream ());
+        out = new DataOutputStream (socket.getOutputStream ());
+
+        isRunning = true;
+
+        // потоки In and Out
+        Thread threeIn = new Thread (() -> {
+            System.out.println ("Пользователь подключился");
+            try {
+                while (isRunning) {
+                    if (!socket.isConnected ()) {
+                        System.out.println ("Пользователь вышел");
+                        isRunning = false;
+                        break;
+                    }
+
+                    String str = in.readUTF ();
+
+                    if (str.equalsIgnoreCase ("/end")) {
+                        System.out.println ("Пользователь вышел");
+                        isRunning = false;
+                        break;
+                    }
+
+                    System.out.println ("Клиент пишет:" + str);
+                }
+            } catch (IOException e) {
+                e.printStackTrace ();
+            }
+        });
+        Thread fourOut = new Thread (() -> {
+            Scanner sc = new Scanner (System.in);
+            try {
+                while (isRunning) {
+                    if (!socket.isConnected ()) {
+                        isRunning = false;
+                        break;
+                    }
+
+                    String str = sc.nextLine ();
+                    out.writeUTF (str);
+
+                    if (str.equals ("/end")) {
+                        isRunning = false;
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace ();
+            }
+        });
+        // запуск потоков
+        threeIn.start ();
+        fourOut.start ();
+
+        try {
+            threeIn.join ();
+            fourOut.join ();
+        } catch (InterruptedException e) {
+            e.printStackTrace ();
+        }
+    }
+
+    public static void closeConnection() throws IOException {
+        in.close ();
+        out.close ();
+
+        if (!socket.isClosed ()) {
+            socket.close ();
+        }
+        if (!serverSocket.isClosed ()) {
+            serverSocket.close ();
+        }
+    }
 }
